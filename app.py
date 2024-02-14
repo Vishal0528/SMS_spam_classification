3a7114894155ec97c23a3c978610a2a956d981ed
Vishal Dev <83056247+Vishal0528@users.noreply.github.com>
2024-02-14 14:09:33 +0530

    Update app.py
    updated to text_area("Enter your message) from text text_input("Enter your message")

--- app.py
+++ app.py
@@ -30,7 +30,7 @@ def transform_text(text):
 tfidf=pickle.load(open('vectorizer.pkl','rb'))
 model=pickle.load(open('model.pkl','rb'))
 st.title("Email/SMS Spam Classifier")
-input_sms=st.text_input("Enter the message")
+input_sms=st.text_area("Enter the message")
 if st.button('Predict'):
 
     # 1. Preprocess
